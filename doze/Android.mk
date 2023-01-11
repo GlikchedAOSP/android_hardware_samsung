4aa41eae88c923571abdc8000dbb81f43bdb3ac2
glikched <florianvictorcristea@gmail.com>
2023-01-11 09:14:11 +0000

    hardware/samsung: Remove need of lineage-sdk by adding only needs
    *ScreenType from https://github.com/LineageOS/android_lineage-sdk/blob/lineage-20.0/sdk/src/java/org/lineageos/internal/util/ScreenType.java

--- doze/Android.mk
+++ doze/Android.mk
@@ -15,9 +15,6 @@ LOCAL_USE_AAPT2 := true
 LOCAL_STATIC_ANDROID_LIBRARIES := \
     SettingsLib
 
-LOCAL_STATIC_JAVA_LIBRARIES := \
-    org.lineageos.platform.internal
-
 LOCAL_PROGUARD_FLAG_FILES := proguard.flags
 
 LOCAL_RESOURCE_DIR := \
